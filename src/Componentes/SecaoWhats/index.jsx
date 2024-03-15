4fa0cd4ea25eaac51d27491c3caea449d94a7801
MarcosGardinali <marcosgardinali21@outlook.com>
2024-03-15 19:19:17 -0300

    Insert hr tag

--- src/Componentes/SecaoWhats/index.jsx
+++ src/Componentes/SecaoWhats/index.jsx
@@ -6,6 +6,7 @@ import style from './secaowhats.module.css'
 export default function SecaoWhats() {
   return (
     <section className={style.conteudo__secao__whats}>
+      <hr className={style.linha}/>
         <h1 className={style.titulo__secao__whats}>Tire todas as suas dúvidas com a nossa consultoria!</h1>
         <p className={style.texto__secao__whats}>Se você possui dúvidas e deseja falar direto com uma de nossas consultoras, disponibilizamos um Whatsapp Exclusivo.</p>
         <a href="https://api.whatsapp.com/send/?phone=5514998622783&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className={style.conteudo__whatsapp__button}>
