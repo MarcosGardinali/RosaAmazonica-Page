014372a5a00db08d41171c0eef28d120e1ea1b74
MarcosGardinali <marcosgardinali21@outlook.com>
2024-03-15 22:14:24 -0300

    Whats link

--- src/Componentes/SecaoWhats/index.jsx
+++ src/Componentes/SecaoWhats/index.jsx
@@ -9,7 +9,7 @@ export default function SecaoWhats() {
       <hr className={style.linha}/>
         <h1 className={style.titulo__secao__whats}>Tire todas as suas dúvidas com a nossa consultoria!</h1>
         <p className={style.texto__secao__whats}>Se você possui dúvidas e deseja falar direto com uma de nossas consultoras, disponibilizamos um Whatsapp Exclusivo.</p>
-        <a href="https://api.whatsapp.com/send/?phone=5514998622783&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className={style.conteudo__whatsapp__button}>
+        <a href="https://api.whatsapp.com/send/?phone=5514998622783&text=Olá! Tenho interesse em conhecer o Sérum Rosa Amazônica!&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className={style.conteudo__whatsapp__button}>
             <img src={whats} alt="Botão de WhatsApp" />
             <p>CHAMAR NO WHATSAPP</p>
         </a>
